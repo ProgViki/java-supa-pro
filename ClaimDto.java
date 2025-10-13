17e5189638f4b8eb749b1336bd508d44f128606e
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-10-13 21:44:09 +0100

    Update ClaimDto.java 1.1

--- ClaimDto.java
+++ ClaimDto.java
@@ -1,1 +1,11 @@
 
+package com.example.auth.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ClaimDto {
+    private String title;
+    private String description;
+    private double amount;
+}
