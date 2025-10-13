89044e88ea065696f4d3c49c2f461f74476ba45a
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-10-13 21:32:24 +0100

    Update UserRepository.java 1.1

--- UserRepository.java
+++ UserRepository.java
@@ -1,1 +1,11 @@
 
+package com.example.auth.repository;
+
+import com.example.auth.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<AppUser, String> {
+    Optional<AppUser> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
