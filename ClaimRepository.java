9abb5be484ce7f89f22db3d56aeb828e0c56e469
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-10-13 21:43:30 +0100

    Update ClaimRepository.java

--- ClaimRepository.java
+++ ClaimRepository.java
@@ -1,1 +1,11 @@
+package com.example.auth.repository;
+
+import com.example.auth.entity.Claim;
+import com.example.auth.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ClaimRepository extends JpaRepository<Claim, Long> {
+    List<Claim> findByEmployee(AppUser employee);
+}
 
